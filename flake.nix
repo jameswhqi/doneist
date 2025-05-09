cdc3db0aee68436f30992d8d321e6e4ee0dbe467
Wenhao Qi <jameswhqi@gmail.com>
2025-05-09 10:44:21 +0200

    create next app

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "doneist";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devshells.default = {
+            packages = with pkgs; [
+              nodejs
+              pnpm
+            ];
+          };
+        };
+    };
+}
